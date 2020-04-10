36dfb0960b873b9950db6f08a9fd2c8273f24566
804141892@qq.com <804141892@qq.com>
2020-04-10 17:51:17 +0800

    save

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <van-tabbar-item name="home" icon="home-o" replace to="/">首页</van-tabbar-item>
       <van-tabbar-item name="search" icon="search" replace to="/about">分类</van-tabbar-item>
       <van-tabbar-item name="friends" icon="friends-o" replace to="/test">购物车</van-tabbar-item>
-      <van-tabbar-item name="setting" icon="setting-o">我的</van-tabbar-item>
+      <van-tabbar-item name="setting" icon="setting-o" replace to="/login">未登录</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,28 +1,54 @@
 <template>
-    <div>
-        <van-form @submit="onSubmit">
-            <van-field v-model="phone" name="手机号" placeholder="请输入手机号" />
-        </van-form>
-    </div>
+  <div>
+    <van-form @submit="onSubmit">
+      <van-field v-model="phone" label-align="right" label="手机号" placeholder="请输入手机号" 
+        :rules="[{ phoneValidator , phoneVM }]"
+      />
+      <van-field v-model="code" label-align="right" center clearable label="短信验证码"
+        :rules="[{ codeValidator , codeVM }]"
+       placeholder="请输入短信验证码">
+        <template #button>
+          <van-button size="small" type="primary">发送验证码</van-button>
+        </template>
+      </van-field>
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">提交</van-button>
+      </div>
+    </van-form>
+  </div>
 </template>
 
 <script>
+import { Form, Field, Button } from "vant";
+
 export default {
-    name:'Login',
+  name: "Login",
+  components: {
+    [Form.name]: Form,
+    [Field.name]: Field,
+    [Button.name]: Button
+  },
+  data() {
+    return {
+      phone: "",
+      code: "",
 
-    data(){
-        return{
-            phone:'',
-            code:'',
+      phoneVM:"",
+      codeVM:"",
+    };
+  },
 
-        }
+  methods: {
+    //登录
+    onSubmit() {},
+
+    phoneValidator(val){
+
     },
 
-    methods:{
-        //登录
-        onSubmit(){
+    codeValidator(val){
 
-        }
     }
-}
+  }
+};
 </script>
